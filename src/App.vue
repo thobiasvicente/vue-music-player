dbf28a7ebea8970964078e0e9f2938c20ec0b4b3
thobias.vicente <thobias.vicente@zemis.com.br>
2020-12-05 13:10:50 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,118 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <main id="main">
+      <transition mode="out-in">
+        <router-view />
+      </transition>
+    </main>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0px;
+  margin: 0px;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+ul {
+  list-style: none;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: #345;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
+
+#app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  background-color: #f0eded;
+}
+
+#main {
+  flex: 1;
+}
+
+label {
+  margin-bottom: 5px;
+}
+
+input,
+textarea {
+  border-radius: 4px;
+  border: 1px solid white;
+  padding: 15px;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  transition: all 0.3s;
+  font-size: 1rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  margin-bottom: 15px;
+}
+
+input:hover,
+input:focus,
+textarea:hover,
+textarea:focus {
+  outline: none;
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  border-color: #87f;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter {
+  transform: translate3d(0, -20px, 0);
+}
+
+.v-leave-to {
+  transform: translate3d(0, 20px, 0);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.3s;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,197 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <section class="player">
+      <h2 class="song-title">
+        {{ current.title }} - <span>{{ current.artist }}</span>
+      </h2>
+      <div class="controls">
+        <button class="prev" @click="prev">Prev</button>
+        <button class="play" v-if="!isPlaying" @click="play">Play</button>
+        <button class="pause" v-else @click="pause">Pause</button>
+        <button class="next" @click="next">Next</button>
+      </div>
+    </section>
+    <section class="playlist">
+      <h3>The Playlist</h3>
+      <button
+        v-for="song in songs"
+        :key="song.src"
+        @click="play(song)"
+        :class="song.src == current.src ? 'song playing' : 'song'"
+      >
+        {{ song.title }} - {{ song.artist }}
+      </button>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
 @Component({
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  //this is equivalent of data()
+  private current = new Object();
+  private index = 0;
+  private isPlaying = false;
+  private songs: Array<object> = [
+    {
+      title: "Grateful",
+      artist: "Neffex",
+      src: require("@/assets/neffex-grateful.mp3")
+    },
+    {
+      title: "Invincible",
+      artist: "Deaf Kev",
+      src: require("@/assets/deaf-kev-invincible.mp3")
+    }
+  ];
+  private player = new Audio();
+
+  //this is equivalent of methods()
+  public play(song: { src?: unknown }): void {
+    if (typeof song.src != "undefined") {
+      this.current = song;
+      this.player.src = this.current.src;
+    }
+    this.player.play();
+    this.player.addEventListener(
+      "ended",
+      function() {
+        this.index++;
+        if (this.index > this.songs.length - 1) {
+          this.index = 0;
+        }
+        this.current = this.songs[this.index];
+        this.play(this.current);
+      }.bind(this)
+    );
+    this.isPlaying = true;
+    document.title = this.current.title;
+  }
+  public pause(): void {
+    this.player.pause();
+    this.isPlaying = false;
+  }
+  public next(): void {
+    this.index++;
+    if (this.index > this.songs.length - 1) {
+      this.index = 0;
+    }
+    this.current = this.songs[this.index];
+    this.play(this.current);
+  }
+  public prev(): void {
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.songs.length - 1;
+    }
+    this.current = this.songs[this.index];
+    this.play(this.current);
+  }
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  }
+}
 </script>
+<style scoped>
+.home {
+  margin-top: 20px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+}
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #fff;
+}
+main {
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
+  padding: 25px;
+}
+.song-title {
+  color: #53565a;
+  font-size: 32px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+}
+.song-title span {
+  font-weight: 400;
+  font-style: italic;
+}
+.controls {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px 15px;
+}
+button {
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+  cursor: pointer;
+}
+button:hover {
+  opacity: 0.8;
+}
+.play,
+.pause {
+  font-size: 20px;
+  font-weight: 700;
+  padding: 15px 25px;
+  margin: 0px 15px;
+  border-radius: 8px;
+  color: #fff;
+  background-color: #cc2e5d;
+}
+.next,
+.prev {
+  font-size: 16px;
+  font-weight: 700;
+  padding: 10px 20px;
+  margin: 0px 15px;
+  border-radius: 6px;
+  color: #fff;
+  background-color: #ff5858;
+}
+.playlist {
+  padding: 0px 30px;
+}
+.playlist h3 {
+  color: #212121;
+  font-size: 28px;
+  font-weight: 400;
+  margin-bottom: 30px;
+  text-align: center;
+}
+.playlist .song {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+}
+.playlist .song:hover {
+  color: #ff5858;
+}
+.playlist .song.playing {
+  color: #fff;
+  background-image: linear-gradient(to right, #cc2e5d, #ff5858);
+}
+</style>
